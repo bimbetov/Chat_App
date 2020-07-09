a14af4828871fc359b87528eefae3f5cb5284d4b
bimbetov <beimbik-00@mail.ru>
2020-07-09 14:56:15 +0600

    Adding functionality for working with Firebase + adding the BubbleView library

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,14 +1,96 @@
 package com.example.chatapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.github.library.bubbleview.BubbleTextView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SIGN_IN_CODE = 1;
+    private RelativeLayout room_activity;
+    private FirebaseListAdapter<Message> adapter;
+    private FloatingActionButton sendBtn;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN_CODE){
+            if (resultCode == RESULT_OK){
+                Snackbar.make(room_activity, "Вы авторизованы", Snackbar.LENGTH_LONG).show();
+                displayAllMessages();
+            }
+            else {
+                Snackbar.make(room_activity, "Вы не авторизованы", Snackbar.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        room_activity = findViewById(R.id.room_activity);
+        sendBtn = findViewById(R.id.btnSend);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText textField = findViewById(R.id.messageField);
+                if (textField.getText().toString().equals(""))
+                    return;
+
+                FirebaseDatabase.getInstance().getReference().push().setValue(
+                        new Message(FirebaseAuth.getInstance().getCurrentUser().getEmail(),
+                                textField.getText().toString()
+                        )
+                );
+                textField.setText("");
+            }
+        });
+
+        //Пользователь еще не авторизован
+        if(FirebaseAuth.getInstance().getCurrentUser() == null)
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE);
+        else {
+            Snackbar.make(room_activity, "Вы авторизованы", Snackbar.LENGTH_LONG).show();
+            displayAllMessages();
+        }
+    }
+
+    private void displayAllMessages() {
+        ListView listOFMessages = findViewById(R.id.list_of_message);
+        adapter = new FirebaseListAdapter<Message>(this, Message.class, R.layout.list_item, FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, Message model, int position) {
+                TextView mess_user, mess_time;
+                BubbleTextView mess_text;
+                mess_user = v.findViewById(R.id.message_user);
+                mess_time = v.findViewById(R.id.message_time);
+                mess_text = v.findViewById(R.id.message_text);
+
+                mess_user.setText(model.getUserName());
+                mess_text.setText(model.getTextMessage());
+                mess_time.setText(DateFormat.format("HH:mm (dd MMM yyyy)", model.getMessageTime()));
+
+            }
+        };
+
+        listOFMessages.setAdapter(adapter);
     }
 }
