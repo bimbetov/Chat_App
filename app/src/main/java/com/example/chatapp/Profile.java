743c806b6ac9019b16c5348df5746e05a1e4e2b0
bimbetov <beimbik-00@mail.ru>
2020-07-17 19:28:57 +0600

    Adding profile photo editing

--- app/src/main/java/com/example/chatapp/Profile.java
+++ app/src/main/java/com/example/chatapp/Profile.java
@@ -1,25 +1,179 @@
 package com.example.chatapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import de.hdodenhof.circleimageview.CircleImageView;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Profile extends AppCompatActivity {
+
+    CircleImageView mImageView;
+    Button mChooseBtn;
+
+    private Uri imageData;
+    private StorageReference folder;
+    private HashMap<String, String> mapForUserImg;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_info);
 
+        folder = FirebaseStorage.getInstance().getReference("ImageFolder");
+
+        userDataInitialization();
+
+        mChooseBtn = findViewById(R.id.choose_image_btn);
+        mChooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    } else {
+                        pickImageFromGallery();
+                    }
+                }
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+        initialBottomNavigationMenu(bottomNavigationView);
+    }
+
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery();
+                } else {
+                    Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            imageData = data.getData();
+            mImageView.setImageURI(imageData);
+
+            final StorageReference imageName = folder.child("image" + imageData.getLastPathSegment());
+            imageName.putFile(imageData).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    imageName.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            DatabaseReference imageStore = FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                            HashMap<String, String> map = new HashMap<>();
+                            map.put("imageUrl", String.valueOf(uri));
+
+                            imageStore.setValue(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(Profile.this, "Finally completed", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+
+    }
+
+    private void initialBottomNavigationMenu(BottomNavigationView bottomNavigationView) {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.add:
+                    case R.id.profile:
+                        break;
+                    case R.id.chats:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void userDataInitialization() {
         TextView profileTitle = findViewById(R.id.titleTextForProfile);
         TextView userName_textView = findViewById(R.id.name_textView);
         TextView userEmail_textView = findViewById(R.id.email_textView);
+        mImageView = findViewById(R.id.person_image);
+
+        DatabaseReference mRefForUserImg = FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        mRefForUserImg.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mapForUserImg = (HashMap<String, String>) dataSnapshot.getValue();
+                if (mapForUserImg != null) {
+                    String url = mapForUserImg.get("imageUrl");
+
+                    Glide
+                            .with(Profile.this)
+                            .load(url)
+                            .centerCrop()
+                            .into(mImageView);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         String userName = getIntent().getStringExtra("USER_NAME");
         String userEmail = getIntent().getStringExtra("USER_EMAIL");
@@ -27,27 +181,5 @@ public class Profile extends AppCompatActivity {
         profileTitle.setText(userName);
         userName_textView.setText(userName);
         userEmail_textView.setText(userEmail);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.profile);
-        initialBottomNavigationMenu(bottomNavigationView);
-    }
-    private void initialBottomNavigationMenu(BottomNavigationView bottomNavigationView) {
-        //bottomNavigationView.setSelectedItemId(R.id.chats);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.add:
-                    case R.id.profile:
-                        break;
-                    case R.id.chats:
-                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        overridePendingTransition(0,0);
-                        break;
-                }
-                return false;
-            }
-        });
     }
 }
